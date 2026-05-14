cc62c004c3a7b7dd40668fd330c679fb67f34bc0
StalkerHacker00237 <ngingayafrancois@gmail.com>
2026-05-14 11:44:54 +0200

    fix railway host

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ const PORT = process.env.PORT || 5000;
 
 console.log("ENV CHECK:", process.env.NOTCHPAY_PUBLIC_KEY);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
